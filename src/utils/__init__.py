c6df11438ba98f90ace9c6d98769aabd2b4a321e
Vanh41 <vietanhm6a@gmail.com>
2025-12-26 19:21:27 +0700

    del unimportant files and folders

--- src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .file_utils import save_to_json, load_from_json
-from .logger import setup_logger
--- src/utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def setup_logger(name="training_logger", level=logging.INFO):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-    logger.addHandler(handler)
-    return logger
